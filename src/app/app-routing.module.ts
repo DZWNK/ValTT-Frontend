77cf71a69c291b3aea7e60b6389513829b1cc1ca
Daniel Kerr <dkerr10@myseneca.ca>
2021-01-03 13:51:40 -0500

    Component template creation and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventComponent } from './event/event.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { HomeComponent } from './home/home.component';
+import { MatchComponent } from './match/match.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PlayerComponent } from './player/player.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TeamListComponent } from './team-list/team-list.component';
+import { TeamComponent } from './team/team.component';
 
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
 
-const routes: Routes = [];
+  {path: 'profile/:id', component: ProfileComponent},
+  {path: 'profile/:id/favourites', component: FavouritesComponent},
+  {path: 'player/:id', component: PlayerComponent},
+  {path: 'team/:id', component: TeamComponent},
+  {path: 'event/:id', component: EventComponent},
+  {path: 'match/:id', component: MatchComponent},
+
+  {path: 'team_list', component: TeamListComponent},
+  {path: 'event_list', component: EventListComponent},
+
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { PlayerComponent } from './player/player.component';
+import { TeamComponent } from './team/team.component';
+import { EventComponent } from './event/event.component';
+import { MatchComponent } from './match/match.component';
+import { TeamListComponent } from './team-list/team-list.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProfileComponent,
+    FavouritesComponent,
+    PlayerComponent,
+    TeamComponent,
+    EventComponent,
+    MatchComponent,
+    TeamListComponent,
+    EventListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
