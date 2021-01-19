8e6e62daa2008a585e66b40ebc8bea3f8148b774
Daniel Kerr <dkerr10@myseneca.ca>
2021-01-19 12:18:35 -0500

    added Header and Footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { MatchComponent } from './match/match.component';
 import { TeamListComponent } from './team-list/team-list.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     MatchComponent,
     TeamListComponent,
     EventListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
