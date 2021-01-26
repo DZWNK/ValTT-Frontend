27d2d962a013135aa694b44cbcc38f01817044d3
Peter <pdzwonek@myseneca.ca>
2021-01-26 17:18:26 -0500

    Seperated login and signup components from header

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Profile } from '../../models/Profile';
+import { UserManagerService } from '../user-manager.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  signupDisplay = "none";
+  loginDisplay = "none";
+  
+  loginData: Profile;
+  signupData: Profile;
 
-  constructor() { }
+  constructor(private userManagerService: UserManagerService) { }
 
   ngOnInit(): void {
+
+    this.onLoginClick();
+    this.loginData = {
+      username: "",
+      password: ""
+    }
+
+    this.signupData ={
+      username: "",
+      password: "",
+    }
+  }
+
+  onLoginClick(): void{
+    this.loginDisplay = "block";
+  }
+
+  onLoginClose(){
+    this.loginDisplay = "none";
+    this.loginData = {
+      username: "",
+      password: ""
+    }
+  }
+
+  onLoginSubmit(){
+    console.log("login-submit");
+    // Hookup data to API for handling
+    this.userManagerService.signup(this.signupData).subscribe(msg => {
+      console.log(msg);
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Profile } from '../../models/Profile';
+import { UserManagerService } from '../user-manager.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  
 
-  constructor() { }
+  signupDisplay = "none";
+  loginData: Profile;
+  signupData: Profile;
+
+  constructor(private userManagerService: UserManagerService) { }
 
   ngOnInit(): void {
+    this.onSignupClick();
+    this.loginData = {
+      username: "",
+      password: ""
+    }
+
+    this.signupData ={
+      username: "",
+      password: "",
+    }
   }
 
+  onSignupClick(): void{
+    this.signupDisplay = "block";
+  }
+
+  onSignupClose(): void{
+    this.signupDisplay = "none";
+    this.signupData = {
+      username: "",
+      password: ""  
+    }
+  }
+
+  onSignupSubmit(){
+    console.log("signup-submit");
+    // Hookup data to API for handling
+    this.userManagerService.login(this.loginData).subscribe(msg => {
+      console.log(msg);
+    }, err => {
+      console.log(err);
+    });
+  }
 }
